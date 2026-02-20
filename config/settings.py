fad86b4d3b1613adf8daa103f8c4926f464807ec
Олег Навоев <navoev.oleg@gmail.com>
2026-02-20 16:06:36 +0500

    добавление html файлов, регистрация путей и создание контролеров

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "catalog"
+    "catalog",
 ]
 
 MIDDLEWARE = [
@@ -78,9 +78,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+#DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
