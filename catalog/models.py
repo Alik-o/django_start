4de3fa6121ac29c2ca417fb3a54cffaea433300c
Олег Навоев <navoev.oleg@gmail.com>
2026-03-03 20:28:35 +0500

    создание моделей

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='описание')
+    image = models.ImageField(upload_to='images/', verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата изменения')
