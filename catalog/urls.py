3da342ee635ba4712ffac0567ef3adc9f3a6cff9
Олег Навоев <navoev.oleg@gmail.com>
2026-03-16 15:09:06 +0500

    добавлены маршруты и контролеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import contacts, home, products, product_info
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
+    path("products/<int:pk>", products, name="products"),
+    path("product_info/<int:pk>", product_info, name="product_info"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
@@ -11,3 +15,15 @@ def contacts(request):
         name = request.POST.get("name")
         return HttpResponse(f"Здравствуйте, {name}! Ваше сообщение принято!")
     return render(request, "contacts.html")
+
+
+def products(request, pk):
+    products = Product.objects.filter(category_id=pk)
+    context = {'products': products}
+    return render(request, "products.html", context)
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, "product_info.html", context)
