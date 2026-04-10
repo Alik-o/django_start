3aebd44784e3566bfcb905cb4cae1f0c10333593
Олег Навоев <navoev.oleg@gmail.com>
2026-04-10 15:37:26 +0500

    добавил / в конце url адресов

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("blog/", BlogListView.as_view(), name="blog_list"),
-    path("blog/<int:pk>", BlogDetailView.as_view(), name="blog_detail"),
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("blog/blog_create/", BlogCreateView.as_view(), name="blog_form"),
     path("blog/<int:pk>/update/", BlogUpdateView.as_view(), name="blog_update"),
     path("blog/<int:pk>/delete/", BlogDeleteView.as_view(), name="blog_delete"),
--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", CategoryListView.as_view(), name="category_list"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
-    path("products/<int:pk>", ProductListView.as_view(), name="products"),
-    path("product_info/<int:pk>", ProductDetailView.as_view(), name="product_info"),
+    path("products/<int:pk>/", ProductListView.as_view(), name="products"),
+    path("product_info/<int:pk>/", ProductDetailView.as_view(), name="product_info"),
 ]
