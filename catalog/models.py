e5e3f0f6b9caa0797e6bb5259bd9c8be4df74e2d
Олег Навоев <navoev.oleg@gmail.com>
2026-04-25 15:05:59 +0500

    добавлен владелец продукта и статус публикации

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(null=True, blank=True, verbose_name='описание')
@@ -17,11 +18,13 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(null=True, blank=True, verbose_name='описание')
-    image = models.ImageField(upload_to='images/', verbose_name='изображение', null=True, blank=True,)
+    image = models.ImageField(upload_to='images/', verbose_name='изображение', null=True, blank=True, )
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product', verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата изменения')
+    publication_status = models.BooleanField(default=False, verbose_name='статус публикации')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='владелец', related_name='product')
 
     def __str__(self):
         return f'{self.name}'
@@ -30,3 +33,6 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['name']
+        permissions = (
+            ('can_publish_product', 'Может опубликовать продукт'),
+        )
--- catalog/views.py
+++ catalog/views.py
@@ -39,6 +39,10 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     context_object_name = 'product'
     success_url = reverse_lazy('catalog:category_list')
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
